c4fc3824efb5e8b10c40b586670692658ee35bb9
Omarfaruk4200 <emon35293@gmail.com>
2022-03-27 19:35:25 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+// console.log("hello")
+// db = {}
+
+// const addTodb = item => {
+//     db[item] = 1
+//     console.log(db)
+// }
+
+
+const coke = {}
+
+const addtoCoke = product => {
+    coke[product] = 2
+    console.log(coke)
+}
+
+const divise = {}
+const addtoDivise = item => {
+    divise[item] = 5
+    // console.log("hello")
+    console.log(divise)
+}
+
+
+
+
+
+
+
+
